b445de45199d7df608bc83a9698f1276841dc309
Rahul Jangra <dev.rahuljangra@gmail.com>
2025-02-28 22:27:38 +0530

    - Anagram String - Anagram Search

--- src/PODGfg/GFGStringProblems.java
+++ src/PODGfg/GFGStringProblems.java
@@ -120,4 +120,60 @@ public class GFGStringProblems {
         }
     }
 
+    /**
+     * Anagram Strings
+     */
+    public boolean areAnagram(String s1, String s2) {
+        int[] count = new int[26];  //assume all characters are in lowercase
+
+        for (int i = 0; i < s1.length(); i++) {
+            count[(s1.charAt(i) - 'a')]++;    //count[s1.charAt(i) - 'a'] = count[s1.charAt(i) - 'a'] + 1;
+        }
+
+        //decrease the count w.r.t character
+        for (int i = 0; i < s2.length(); i++) {
+            count[(s2.charAt(i) - 'a')]++;    //count[s2.charAt(i) - 'a'] = count[s2.charAt(i) - 'a'] - 1;
+        }
+
+        //Now check if any value in count array is not 0 means that character not present in s2.
+        for (int j : count) {
+            if (j != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Anagram Search
+     */
+
+    static final int CHAR = 256;
+
+    public boolean anagramSearch(String txt, String pat) {
+        int n = txt.length(), m = pat.length();
+
+        int[] countTxt = new int[CHAR];
+        int[] countPat = new int[CHAR];
+
+        for (int i = 0; i < m; i++) {
+            countTxt[i]++;
+            countPat[i]++;
+        }
+        for (int i = pat.length(); i < n; i++) {
+            if (areSame(countTxt, countPat)) return true;
+            countTxt[txt.charAt(i)]++;
+            countTxt[txt.charAt(i - pat.length())]--;
+        }
+        return false;
+    }
+
+    static boolean areSame(int[] CT, int[] CP) {
+        for (int i = 0; i < CHAR; i++) {
+            if (CT[i] != CP[i]) return false;
+        }
+        return true;
+    }
+
 }
